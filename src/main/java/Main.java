1d57fa18a5dc9b9832f0d256f2982ea94e61f01b
Bram de Gooijer <degooijerbram@gmail.com>
2022-09-08 19:33:06 +0200

    tests voor p2 gemaakt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import Domein.Reiziger;
+import Interface.ReizigerDAO;
 import Interface.ReizigerDAOsql;
 
 import java.sql.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -22,9 +24,55 @@ public class Main {
 //            System.out.println(reizigerDAOsql.findByGbDatum("2002-12-03"));
 //            System.out.println(reizigerDAOsql.findAll());
 
+            testReizigerDAO(reizigerDAOsql);
+
         } catch (Exception e) {
             System.out.println("Something went wrong!");
             System.out.println(e.getMessage());
         }
+    }
+
+    /**
+     * P2. Reiziger DAO: persistentie van een klasse
+     *
+     * Deze methode test de CRUD-functionaliteit van de Reiziger DAO
+     *
+     * @throws SQLException
+     */
+    private static void testReizigerDAO(ReizigerDAO rdao) throws SQLException {
+        System.out.println("\n---------- Test ReizigerDAO -------------");
+
+        // Haal alle reizigers op uit de database
+        List<Reiziger> reizigers = rdao.findAll();
+        System.out.println("[Test] ReizigerDAO.findAll() geeft de volgende reizigers:");
+        for (Reiziger r : reizigers) {
+            System.out.println(r);
+        }
+        System.out.println();
+
+        // Maak een nieuwe reiziger aan en persisteer deze in de database
+        String gbdatum = "1981-03-14";
+        Reiziger sietske = new Reiziger(77, "S", "", "Boers", java.sql.Date.valueOf(gbdatum));
+        System.out.print("[Test] Eerst " + reizigers.size() + " reizigers, na ReizigerDAO.save() ");
+        rdao.save(sietske);
+        reizigers = rdao.findAll();
+        System.out.println(reizigers.size() + " reizigers\n");
+
+        // Voeg aanvullende tests van de ontbrekende CRUD-operaties in.
+        // Update een bestaande reiziger en persisteer deze in de database
+        Reiziger updatedReiziger = new Reiziger(50, "Bram", "de", "Gooijer", Date.valueOf("2004-01-26"));
+        System.out.println(String.format("[Test] Eerst is de Reiziger:\n %s\n -----En na het updaten is het-----", rdao.findById(50)));
+        rdao.update(updatedReiziger);
+        System.out.println(rdao.findById(50));
+
+        //Verwijder een reiziger uit de db
+        Reiziger deletedReiziger = new Reiziger(52, "Jeroen", null, "Fredrikzs", Date.valueOf("2000-01-01"));
+        rdao.save(deletedReiziger);
+        System.out.println(String.format("\n[Test] Eerst is de lijst met alle reizigers:\n%s", rdao.findAll()));
+        rdao.delete(deletedReiziger);
+        System.out.println(String.format("[Test] Daarna ziet de lijst er zo uit:%s", rdao.findAll()));
+
+        // Find by geboortedatum
+        System.out.println(String.format("[Test] Hier alle reizigers met de geboortedatum 2002-12-03:\n%s", rdao.findByGbDatum("2002-12-03")));
     }
 }
